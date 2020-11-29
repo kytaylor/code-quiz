3b3ca36cfd376022f49c9fa3d89541ce93865f55
kytaylor <ky.taylor5468@gmail.com>
2020-11-29 15:38:42 -0800

    adds screen that will show final score, has non functional text box for name and adding the score to the scoreboard. Adds styling throughout

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ var qFive = document.getElementById("question-five");
 var qSix = document.getElementById("question-six");
 
 // Answer buttons
-var q1Buttons = document.querySelector("#q1-button");
-var q1CorrectButton = document.querySelector("#correct-q1");
+var q1Buttons = document.querySelectorAll(".q1-button");
+var q1CorrectButton = document.querySelectorAll(".correct-q1");
 
 // Other variables
 var menu = document.getElementById("start-menu");
@@ -18,6 +18,8 @@ var start = document.querySelector(".start-button");
 
 var correct = document.getElementById("correct");
 var incorrect = document.getElementById("incorrect");
+
+var scoreScreen = document.getElementById("score-screen");
 
 var timer = document.querySelector(".time");
 var secondsLeft = 60;
@@ -29,8 +31,11 @@ qThree.hidden = true;
 qFour.hidden = true;
 qFive.hidden = true;
 qSix.hidden = true;
+
 correct.hidden = true;
 incorrect.hidden = true;
+
+scoreScreen.hidden = true;
 
 // Array with all questions
 var questions = [qOne, qTwo, qThree, qFour, qFive, qSix];
@@ -42,24 +47,83 @@ function startQuiz(event) {
     menu.hidden = true;
     qOne.hidden = false;
 
+    for (var i = 0; i < q1Buttons.length; i++) {    
+        q1Buttons[i].addEventListener('click', incorrectAnswer)
+    }
+
+    for (var i = 0; i < q1CorrectButton.length; i++) {    
+        q1CorrectButton[i].addEventListener('click', correctAnswer)
+    }
+
     function correctAnswer(event) {
         event.preventDefault();
 
-        qOne.hidden = true;
-        qTwo.hidden = false;
-        correct.hidden = false;
+        if(qOne.hidden == false) {
+            qOne.hidden = true;
+            qTwo.hidden = false;
+            correct.hidden = false;
+        } else if(qTwo.hidden == false) {
+            qTwo.hidden = true;
+            qThree.hidden = false;
+            incorrect.hidden = true;
+            correct.hidden = false;
+        } else if(qThree.hidden == false) {
+            qThree.hidden = true;
+            qFour.hidden = false;
+            incorrect.hidden = true;
+            correct.hidden = false;
+        } else if(qFour.hidden == false) {
+            qFour.hidden = true;
+            qFive.hidden = false;
+            incorrect.hidden = true;
+            correct.hidden = false;
+        } else if(qFive.hidden == false) {
+            qFive.hidden = true;
+            qSix.hidden = false;
+            incorrect.hidden = true;
+            correct.hidden = false;
+        } else if(qSix.hidden == false) {
+            qSix.hidden = true;
+            scoreScreen.hidden = false;
+            incorrect.hidden = true;
+            correct.hidden = false;
+        }
     }
 
     function incorrectAnswer(event) {
         event.preventDefault();
 
-        qOne.hidden = true;
-        qTwo.hidden = false;
-        incorrect.hidden = false;
+        if(qOne.hidden == false) {
+            qOne.hidden = true;
+            qTwo.hidden = false;
+            incorrect.hidden = false;
+        } else if(qTwo.hidden == false) {
+            qTwo.hidden = true;
+            qThree.hidden = false;
+            correct.hidden = true;
+            incorrect.hidden = false;
+        } else if(qThree.hidden == false) {
+            qThree.hidden = true;
+            qFour.hidden = false;
+            correct.hidden = true;
+            incorrect.hidden = false;
+        } else if(qFour.hidden == false) {
+            qFour.hidden = true;
+            qFive.hidden = false;
+            correct.hidden = true;
+            incorrect.hidden = false;
+        } else if(qFive.hidden == false) {
+            qFive.hidden = true;
+            qSix.hidden = false;
+            correct.hidden = true;
+            incorrect.hidden = false;
+        } else if(qSix.hidden == false) {
+            qSix.hidden = true;
+            scoreScreen.hidden = false;
+            correct.hidden = true;
+            incorrect.hidden = false;
+        }
     }
-
-    q1CorrectButton.addEventListener("click", correctAnswer)
-    q1Buttons.addEventListener("click", incorrectAnswer)
 }
 
 start.addEventListener("click", startQuiz)
