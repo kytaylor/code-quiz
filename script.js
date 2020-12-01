18e7db997e8a10013ae126c2c123d4ec12a729c1
kytaylor <ky.taylor5468@gmail.com>
2020-12-01 13:29:21 -0800

    adds function to take the entered name and final score and set to local storage

--- script.js
+++ script.js
@@ -25,7 +25,10 @@ var finalScore = document.getElementById("final-score");
 var timer = document.querySelector(".time");
 var secondsLeft = 60;
 
-// Makes questions start out not visible
+// Scoreboard variables
+var scoreList = document.getElementById("scores");
+
+// Makes questions, score screen, and correct/incorrect start out not visible
 qOne.hidden = true;
 qTwo.hidden = true;
 qThree.hidden = true;
@@ -41,112 +44,138 @@ scoreScreen.hidden = true;
 // Array with all questions
 var questions = [qOne, qTwo, qThree, qFour, qFive, qSix];
 
+// Event listeners
+start.addEventListener("click", startQuiz);
+document.getElementById("submit-button").addEventListener("click", scoreSubmit);
+
 // Quiz function
-function startQuiz(event) {
+function startQuiz() {
+  // Timer that counts down from 60
+  // Timer stops if time runs out or the end of the quiz is reached
+  // Once the quiz finishes, displays the number left of the timer as the score
+  var timerInterval = setInterval(function () {
+    timer.textContent = secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      qOne.hidden = true;
+      qTwo.hidden = true;
+      qThree.hidden = true;
+      qFour.hidden = true;
+      qFive.hidden = true;
+      qSix.hidden = true;
+      scoreScreen.hidden = false;
+    } else if (scoreScreen.hidden == false) {
+      clearInterval(timerInterval);
+      var endScore = secondsLeft;
+      finalScore.textContent = endScore;
+      console.log(endScore)
+      return endScore;
+    }
+    secondsLeft--;
+  }, 1000);
+
+  menu.hidden = true;
+  qOne.hidden = false;
+
+  // If an incorrect answer is selected, runs incorrect function
+  for (var i = 0; i < q1Buttons.length; i++) {
+    q1Buttons[i].addEventListener("click", incorrectAnswer);
+  }
+
+  // If a correct answer is selected, runs correct function
+  for (var i = 0; i < q1CorrectButton.length; i++) {
+    q1CorrectButton[i].addEventListener("click", correctAnswer);
+  }
+
+  // Correct function that moves to next question and displays "correct" at the bottom of the screen
+  function correctAnswer(event) {
     event.preventDefault();
 
-    var timerInterval = setInterval(function() {
-        timer.textContent = secondsLeft;
-    
-        if (secondsLeft === 0) {
-          clearInterval(timerInterval);
-            qOne.hidden = true;
-            qTwo.hidden = true;
-            qThree.hidden = true;
-            qFour.hidden = true;
-            qFive.hidden = true;
-            qSix.hidden = true;
-            scoreScreen.hidden = false;
-        } else if (scoreScreen.hidden == false) {
-            clearInterval(timerInterval);
-            finalScore.textContent = secondsLeft
-            return secondsLeft;
-        }
-        secondsLeft--;
-    } ,1000)
-
-    menu.hidden = true;
-    qOne.hidden = false;
-
-    for (var i = 0; i < q1Buttons.length; i++) {    
-        q1Buttons[i].addEventListener('click', incorrectAnswer)
+    if (qOne.hidden == false) {
+      qOne.hidden = true;
+      qTwo.hidden = false;
+      correct.hidden = false;
+    } else if (qTwo.hidden == false) {
+      qTwo.hidden = true;
+      qThree.hidden = false;
+      incorrect.hidden = true;
+      correct.hidden = false;
+    } else if (qThree.hidden == false) {
+      qThree.hidden = true;
+      qFour.hidden = false;
+      incorrect.hidden = true;
+      correct.hidden = false;
+    } else if (qFour.hidden == false) {
+      qFour.hidden = true;
+      qFive.hidden = false;
+      incorrect.hidden = true;
+      correct.hidden = false;
+    } else if (qFive.hidden == false) {
+      qFive.hidden = true;
+      qSix.hidden = false;
+      incorrect.hidden = true;
+      correct.hidden = false;
+    } else if (qSix.hidden == false) {
+      qSix.hidden = true;
+      scoreScreen.hidden = false;
+      incorrect.hidden = true;
+      correct.hidden = false;
     }
+  }
 
-    for (var i = 0; i < q1CorrectButton.length; i++) {    
-        q1CorrectButton[i].addEventListener('click', correctAnswer)
+  // Incorrect function that moves to next question, displays "incorrect" at the bottom of the screen, and removes 10 seconds from the timer
+  function incorrectAnswer(event) {
+    event.preventDefault();
+
+    secondsLeft -= 10;
+
+    if (qOne.hidden == false) {
+      qOne.hidden = true;
+      qTwo.hidden = false;
+      incorrect.hidden = false;
+    } else if (qTwo.hidden == false) {
+      qTwo.hidden = true;
+      qThree.hidden = false;
+      correct.hidden = true;
+      incorrect.hidden = false;
+    } else if (qThree.hidden == false) {
+      qThree.hidden = true;
+      qFour.hidden = false;
+      correct.hidden = true;
+      incorrect.hidden = false;
+    } else if (qFour.hidden == false) {
+      qFour.hidden = true;
+      qFive.hidden = false;
+      correct.hidden = true;
+      incorrect.hidden = false;
+    } else if (qFive.hidden == false) {
+      qFive.hidden = true;
+      qSix.hidden = false;
+      correct.hidden = true;
+      incorrect.hidden = false;
+    } else if (qSix.hidden == false) {
+      qSix.hidden = true;
+      scoreScreen.hidden = false;
+      correct.hidden = true;
+      incorrect.hidden = false;
     }
-
-    function correctAnswer(event) {
-        event.preventDefault();
-
-        if(qOne.hidden == false) {
-            qOne.hidden = true;
-            qTwo.hidden = false;
-            correct.hidden = false;
-        } else if(qTwo.hidden == false) {
-            qTwo.hidden = true;
-            qThree.hidden = false;
-            incorrect.hidden = true;
-            correct.hidden = false;
-        } else if(qThree.hidden == false) {
-            qThree.hidden = true;
-            qFour.hidden = false;
-            incorrect.hidden = true;
-            correct.hidden = false;
-        } else if(qFour.hidden == false) {
-            qFour.hidden = true;
-            qFive.hidden = false;
-            incorrect.hidden = true;
-            correct.hidden = false;
-        } else if(qFive.hidden == false) {
-            qFive.hidden = true;
-            qSix.hidden = false;
-            incorrect.hidden = true;
-            correct.hidden = false;
-        } else if(qSix.hidden == false) {
-            qSix.hidden = true;
-            scoreScreen.hidden = false;
-            incorrect.hidden = true;
-            correct.hidden = false;
-        }
-    }
-
-    function incorrectAnswer(event) {
-        event.preventDefault();
-
-        secondsLeft -= 10;
-
-        if(qOne.hidden == false) {
-            qOne.hidden = true;
-            qTwo.hidden = false;
-            incorrect.hidden = false;
-        } else if(qTwo.hidden == false) {
-            qTwo.hidden = true;
-            qThree.hidden = false;
-            correct.hidden = true;
-            incorrect.hidden = false;
-        } else if(qThree.hidden == false) {
-            qThree.hidden = true;
-            qFour.hidden = false;
-            correct.hidden = true;
-            incorrect.hidden = false;
-        } else if(qFour.hidden == false) {
-            qFour.hidden = true;
-            qFive.hidden = false;
-            correct.hidden = true;
-            incorrect.hidden = false;
-        } else if(qFive.hidden == false) {
-            qFive.hidden = true;
-            qSix.hidden = false;
-            correct.hidden = true;
-            incorrect.hidden = false;
-        } else if(qSix.hidden == false) {
-            qSix.hidden = true;
-            scoreScreen.hidden = false;
-            correct.hidden = true;
-            incorrect.hidden = false;
-        }
-    }
+  }
 }
 
-start.addEventListener("click", startQuiz)
+function scoreSubmit(event) {
+  event.preventDefault();
+
+  var name = document.querySelector("#name-input").value;
+  var lastScore = finalScore.textContent;
+
+  if (name === "") {
+    alert("Please enter your name!");
+    return;
+  }
+
+  localStorage.setItem("score-name", name);
+  localStorage.setItem("score-final", lastScore);
+  // window.location.href='/'
+  // renderFinalScore();
+}
